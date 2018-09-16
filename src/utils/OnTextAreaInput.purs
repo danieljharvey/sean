f80d4d8c8a37cbd6a290bc5841743ed434ad4098
Daniel J Harvey <dan.harvey@futurice.com>
2018-09-16 13:53:06 +0100

    Fix text area input

---
+++ src/utils/OnTextAreaInput.purs
@@ -0,0 +1,17 @@
+module App.OnTextAreaInput where
+
+import Prelude
+
+import Data.Maybe (Maybe(..))
+import Hedwig.Foreign (Trait, on)
+import Web.DOM.Node as Node
+import Web.Event.Event as Event
+import Web.HTML.HTMLTextAreaElement as HTMLTextAreaElement
+
+onTextAreaInput :: forall msg. (String -> msg) -> Trait msg
+onTextAreaInput f = on "input" $ \event -> do
+  let maybeTextAreaElement = event # Event.target >>= Node.fromEventTarget >>= HTMLTextAreaElement.fromNode
+  value <- case maybeTextAreaElement of
+    Just inputElement -> HTMLTextAreaElement.value inputElement
+    Nothing -> pure ""
+  pure $ f value
